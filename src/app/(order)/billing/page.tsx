e871f79fcc53f12f46c2b769c1ca0850e86edb4d
Boluwatife Adewusi <bolusarz@gmail.com>
2024-07-12 20:35:27 +0100

    fixed pay button not working

--- src/app/(order)/billing/page.tsx
+++ src/app/(order)/billing/page.tsx
@@ -19,7 +19,10 @@ export default function BillingPage() {
       <div className='grid justify-center pt-8 lg:pb-10'>
         <Section className='bg-white'>
           <div className='grid grid-cols-1 gap-6 lg:grid-flow-col lg:grid-cols-2'>
-            <div className='order-last flex flex-col gap-6 md:order-first'>
+            <div
+              id='details'
+              className='order-last flex scroll-mt-24 flex-col gap-6 md:order-first'
+            >
               <BillingInfoCard />
             </div>
 
