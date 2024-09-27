0e819c0c4a82af379cfd124ff3091da45f297b52
Favour Adekogbe <favouradekogbe@gmail.com>
2024-09-27 14:37:25 +0100

    update not found component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,29 @@
-export const NotFound = () => {
+'use client';
+import { Button } from '@nextui-org/react';
+import { useRouter } from 'next/navigation';
+
+const NotFound = () => {
+  const router = useRouter();
+  const handleBack = () => {
+    router.back();
+  };
+
   return (
-    <main className="p-4">
-      <h1 className="text-center text-lg">Uh-oh, we couldn&apos;t find that page</h1>
+    <main className='flex h-[calc(100dvh-140px)] flex-col items-center justify-center p-4'>
+      <h1 className='mb-6 text-center text-lg'>
+        Uh-oh, we couldn&apos;t find that page
+      </h1>
+      <Button
+        radius='full'
+        size='lg'
+        color='primary'
+        className={'font-medium'}
+        href='/sign-in'
+      >
+        Go back
+      </Button>
     </main>
   );
 };
+
+export default NotFound;
