86624371e71a787114ee7db08a385e611715b50a
Favour Adekogbe <favouradekogbe@gmail.com>
2024-09-27 11:19:53 +0100

    update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     remotePatterns: [
       {
@@ -19,5 +18,5 @@ const nextConfig = {
       //   permanent: true,
       // },
     ];
-  },
+  }
 };
