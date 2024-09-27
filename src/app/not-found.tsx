6106a3a4254bd45d040340e55ae2a1d70a530960
Favour Adekogbe <favouradekogbe@gmail.com>
2024-09-27 13:53:59 +0100

    update 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 export const NotFound = () => {
   return (
     <main className="p-4">
-      <h1 className="text-center text-lg">Uh-oh, we couldn't find that page</h1>
+      <h1 className="text-center text-lg">Uh-oh, we couldn&apos;t find that page</h1>
     </main>
   );
 };
