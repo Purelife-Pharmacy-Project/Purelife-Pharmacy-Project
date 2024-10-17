b1a8abdefcba80534e5e5c982eb02f8b59ae857c
von-moyo <florishadekogbe@gmail.com>
2024-10-17 09:30:39 +0100

    conditional back button render

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,26 @@
+'use client';
 import { Footer } from '@/components/home/Footer';
 import { IconLeftArrow } from '@/components/icons/IconLeftArrow';
 import { LoginForm } from '@/components/login/loginForm';
 import { Card, CardBody } from '@nextui-org/react';
+import { useState } from 'react';
 // import { Suspense } from 'react';
 
 export default function LoginPage() {
+  const [fromDoctorFlow, setFromDoctorFlow] = useState(false);
   return (
     <>
       <main className='mb-10 w-[90%] md:w-[45%] sm:w-[70%] mx-auto h-screen justify-center md:mb-0'>
         <div className='mt-12 flex w-full flex-col gap-6'>
-          <div className='w-fit'>
+          {fromDoctorFlow &&
+            <div
+              onClick={()=>{}}
+              className='w-fit'>
             <IconLeftArrow
               color='#1E272F' />
-          </div>
+          </div>}
           <div className='flex flex-col gap-3'>
-            <h1 className='text-2xl font-bold text-header-100 lg:text-3xl'>
+          <h1 className='text-2xl font-bold text-header-100 lg:text-3xl'>
             Welcome Back
           </h1>
           
