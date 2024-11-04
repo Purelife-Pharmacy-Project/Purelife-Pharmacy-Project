2f419896227d6250ccae12008afb61ef3161e495
von-moyo <florishadekogbe@gmail.com>
2024-11-04 10:40:53 +0100

    adjusted banner styling

--- src/components/quality-home-banner.tsx
+++ src/components/quality-home-banner.tsx
@@ -16,11 +16,11 @@ export const QualityHomeBanner: React.FC<QualityHomeBannerProps> = ({
         className={`flex justify-center bg-[#1E272F] ${backgroundClassName}`}
       >
         <Section
-          className={`grid gap-4 bg-[#1E272F] sm:grid-cols-2 ${backgroundClassName}`}
+          className={`grid gap-4 bg-[#1E272F] sm:grid-cols-[1fr_1.4fr] ${backgroundClassName}`}
         >
           <div className='flex flex-col justify-center pt-10 sm:py-14'>
             <div className='relative flex w-[60%] sm:w-full flex-col justify-between text-[#1E272F] md:h-fit '>
-              <h1 className='mb-4 flex h-auto max-w-[70%] flex-col xl:gap-3 bg-transparent font-bold text-white sm:max-w-[100%] text-3xl md:text-4xl lg:max-w-[650px] xl:text-5xl'>
+              <h1 className='mb-4 flex h-auto max-w-[70%] flex-col xl:gap-3 bg-transparent font-bold text-white sm:max-w-[100%] text-3xl md:text-4xl lg:max-w-[500px] xl:text-5xl'>
                 <span className='whitespace-nowrap'>Quality home</span>
                 <span className='whitespace-nowrap'>healthcare you can</span>
                 <span className='whitespace-nowrap'>trust</span>
