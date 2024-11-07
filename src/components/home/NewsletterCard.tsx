3494fd6670b6c54ea6b06a3da648c3ef2ea07205
von-moyo <florishadekogbe@gmail.com>
2024-11-07 17:22:15 +0100

    newsLetter

--- src/components/home/NewsletterCard.tsx
+++ src/components/home/NewsletterCard.tsx
@@ -6,8 +6,6 @@ import { IconRocket } from '../icons/IconRocket';
 import { Section } from './Section';
 import { inputAuth, inputBorderedGray } from '@/theme';
 import Image from 'next/image';
-import { inputAuth, inputBorderedGray } from '@/theme';
-import Image from 'next/image';
 
 export const NewsLetterCard = () => {
   const handleSubmit = (e: FormEvent) => {
